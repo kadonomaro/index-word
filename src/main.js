77609adbfadc21bc9d0f536a273f9b58ab0ff1dd
kadonomaro <denis.afer@gmail.com>
2019-10-26 18:17:57 +0700

    feat: initial setup

--- src/main.js
+++ src/main.js
@@ -4,7 +4,23 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+import * as firebase from "firebase";
+
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFrujE2RhiNTN5Gw3oMICHsfovZuTHnFo",
+  authDomain: "index-word.firebaseapp.com",
+  databaseURL: "https://index-word.firebaseio.com",
+  projectId: "index-word",
+  storageBucket: "index-word.appspot.com",
+  messagingSenderId: "664834727086",
+  appId: "1:664834727086:web:a7dd48b4f891b8e38c213f"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  runtimeCompiler: true,
+  outputDir: 'docs',
+  css: {
+    modules: true
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/index-word/' : '/'
+}
