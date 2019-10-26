5673b34d1978e8efb32b29fd11e4e087a890d3c5
kadonomaro <denis.afer@gmail.com>
2019-10-26 21:09:49 +0700

    feat: get data from firestore

--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
-import * as firebase from "firebase";
 
 Vue.config.productionTip = false;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFrujE2RhiNTN5Gw3oMICHsfovZuTHnFo",
-  authDomain: "index-word.firebaseapp.com",
-  databaseURL: "https://index-word.firebaseio.com",
-  projectId: "index-word",
-  storageBucket: "index-word.appspot.com",
-  messagingSenderId: "664834727086",
-  appId: "1:664834727086:web:a7dd48b4f891b8e38c213f"
+  apiKey: 'AIzaSyAFrujE2RhiNTN5Gw3oMICHsfovZuTHnFo',
+  authDomain: 'index-word.firebaseapp.com',
+  databaseURL: 'https://index-word.firebaseio.com',
+  projectId: 'index-word',
+  storageBucket: 'index-word.appspot.com',
+  messagingSenderId: '664834727086',
+  appId: '1:664834727086:web:a7dd48b4f891b8e38c213f',
 };
 
 firebase.initializeApp(firebaseConfig);
-
-export const db = firebase.firestore();
-
+const db = firebase.firestore();
+export default db;
 new Vue({
   router,
   store,
