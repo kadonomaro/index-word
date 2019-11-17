57f6a82b1aefb038255b6068b5afbc0cbc249727
kadonomaro <denis.afer@gmail.com>
2019-11-17 17:37:56 +0700

    feat: add paginate for other pages

--- src/views/articles/Newest.vue
+++ src/views/articles/Newest.vue
@@ -3,9 +3,17 @@
     <h2>Новые статьи</h2>
     <app-articles
       v-if="newestArticles.length > 0"
-      :articles="newestArticles"
+      :articles="items"
     />
     <app-empty-article v-else />
+    <paginate
+      v-model="page"
+      :page-count="pageCount"
+      :click-handler="pageChangeHandler"
+      :prev-text="'Назад'"
+      :next-text="'Вперед'"
+      :container-class="'paginate'">
+    </paginate>
   </div>
 </template>
 
@@ -14,11 +22,19 @@
 import AppArticles from '@/components/Articles.vue';
 import AppEmptyArticle from '@/components/EmptyArticle.vue';
 import { mapGetters } from 'vuex';
+import paginationMixin from '@/mixins/pagination.mixin';
+
 export default {
   name: 'newest-articles',
+  mixins: [paginationMixin],
   components: {
     AppArticles,
     AppEmptyArticle
+  },
+  mounted(){
+    setTimeout(() => {
+    this.setupPagination(this.newestArticles)
+    }, 1000);
   },
   computed: {
     ...mapGetters([
--- src/views/articles/Popular.vue
+++ src/views/articles/Popular.vue
@@ -3,9 +3,17 @@
     <h2>Популярные статьи</h2>
     <app-articles
       v-if="popularArticles.length > 0"
-      :articles="popularArticles"
+      :articles="items"
     />
     <app-empty-article v-else />
+    <paginate
+      v-model="page"
+      :page-count="pageCount"
+      :click-handler="pageChangeHandler"
+      :prev-text="'Назад'"
+      :next-text="'Вперед'"
+      :container-class="'paginate'">
+    </paginate>
   </div>
 </template>
 
@@ -14,12 +22,19 @@
 import AppArticles from '@/components/Articles.vue';
 import AppEmptyArticle from '@/components/EmptyArticle.vue';
 import { mapGetters } from 'vuex';
+import paginationMixin from '@/mixins/pagination.mixin';
 
 export default {
   name: 'popular-articles',
+  mixins: [paginationMixin],
   components: {
     AppArticles,
     AppEmptyArticle
+  },
+  mounted(){
+    setTimeout(() => {
+    this.setupPagination(this.popularArticles)
+    }, 1000);
   },
   computed: {
     ...mapGetters([
