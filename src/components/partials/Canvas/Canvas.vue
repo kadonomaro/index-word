909c511801a92a643876f7b3d4bfdd853c57ab55
kadonomaro <denis.afer@gmail.com>
2020-08-04 00:18:44 +0700

    feat: change canvas render

--- src/components/partials/Canvas/Canvas.vue
+++ src/components/partials/Canvas/Canvas.vue
@@ -14,12 +14,7 @@ export default {
 	},
 	mounted() {
 		this.initialSettings();
-		this.drawText({
-			font: 'bold 16px Roboto',
-			colors: ['#9b9b9b','#828282', '#696969', '#505050'],
-			words: this.shuffle(words),
-			delay: window.innerWidth <= 767 ? 500 : 1000
-		});
+		this.drawRects();
 	},
 	methods: {
 		initialSettings() {
@@ -28,48 +23,28 @@ export default {
 			this.$refs.canvas.height = 450;
 		},
 
-		drawText({font, colors, words, delay}) {
-			this.context.font = font;
-			const HEIGHT = 15;
-			let counter = 0;
-			const positions = [];
+		drawRects() {
+			const WIDTH = 50;
+			const HEIGHT = WIDTH;
+			const X_LIMIT = this.$refs.canvas.width / WIDTH;
+			const Y_LIMIT = this.$refs.canvas.height / HEIGHT;
 
-
-			const interval = setInterval(() => {
-					const position = {
-							x: Math.ceil(this.getRandomRange(0, this.$refs.canvas.width) / getTextWidth(words[counter])) * getTextWidth(words[counter]),
-							y: Math.ceil(this.getRandomRange(0, this.$refs.canvas.height) / HEIGHT) * HEIGHT,
-					};
-
-					if (!positions.some(pos => pos.x === position.x && pos.y === position.y)) {
-							this.context.fillStyle = colors[this.getRandomRange(0, colors.length - 1)];
-							this.context.fillText(words[counter], position.x, position.y);
-							counter++;
-					}
-					positions.push(position);
-
-					if (counter >= words.length) {
-							clearInterval(interval);
-					}
-			}, delay);
-
-			const getTextWidth = (text) => {
-				return this.context.measureText(text).width;
+			for (let x = 0; x < X_LIMIT; x++) {
+				for (let y = 0; y < Y_LIMIT; y++) {
+					this.context.rect(x * WIDTH, y * HEIGHT, WIDTH * this.getRandomRange(0.5, 1.5), HEIGHT * this.getRandomRange(0.5, 1.5));
+				}
 			}
+
+			this.context.strokeStyle = '#101010';
+			this.context.lineWidth = 5;
+			this.context.stroke();
+			this.context.fillStyle = '#303030';
+			this.context.fill();
 		},
 
 		getRandomRange(min, max) {
 			return Math.floor(Math.random() * (max - min + 1)) + min;
-		},
-
-		shuffle(array) {
-			for (let i = array.length - 1; i > 0; i--) {
-					let j = Math.floor(Math.random() * (i + 1));
-					[array[i], array[j]] = [array[j], array[i]];
-			}
-			return array;
 		}
-
 	}
 }
 </script>
